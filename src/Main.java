61ed96dadedd8f47ef871b40118f82fe04d8440c
mohammad awad <jozefbernat@azet.sk>
2018-07-10 16:46:52 +0300

    second meessage

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("marhaba");
         System.out.println("hello");
+        System.out.println("ahoj");
     }
 }
