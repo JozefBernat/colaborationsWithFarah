ebf5f07d9bc8fccd537aa62c646c2078fd757e0d
mohammad awad <jozefbernat@azet.sk>
2018-07-11 10:08:48 +0300

    message Jozef

--- src/anotherClass.java
+++ src/anotherClass.java
@@ -3,5 +3,6 @@ public class anotherClass {
     public static void main(String[] args) {
         System.out.println("something else");
         System.out.println("new message in branch1");
+        System.out.println("Jozef message");
     }
 }
