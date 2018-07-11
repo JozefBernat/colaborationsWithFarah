8d6e5e4eb4b3c99f52efe5544145d48a90d6420d
Farah Ashqar <farah.ashqar@mail.utoronto.ca>
2018-07-11 09:34:37 +0300

    added branch

--- src/anotherClass.java
+++ src/anotherClass.java
@@ -2,5 +2,11 @@ public class anotherClass {
 
     public static void main(String[] args) {
         System.out.println("something else");
+        System.out.println("something else");
+        System.out.println("something else");
+        System.out.println("something else");
+        System.out.println("something else");
+        System.out.println("something else");
+        System.out.println("something else");
     }
 }
